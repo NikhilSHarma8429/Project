736395869d6ccba7b2925f9a80c3dbba2158ce94
NikhilSHarma8429 <97504900+NikhilSHarma8429@users.noreply.github.com>
2022-09-12 10:44:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import logo2 from 'C:/Users/Arushi/Downloads/1_4HF_xqCCxBmgXt5PprsGFg-removebg-preview.png'
+import React from 'react'
+import ReactPlayer from 'react-player'
+function App() {
+    return (
+        <>
+            
+            
+            <style src="./App.css"></style>
+            <title>All In One Website</title>
+            <body>
+                <img src={logo2}>
+                </img>
+                <h2 className="name">
+                    All_In_One
+                </h2>
+                <h2 className="nav">Home</h2>
+                <h2 className="nav">Youtube</h2>
+                <h2 className="nav">Songs</h2>
+                <h2 className="nav">Anime</h2>
+                <h2 className="nav">Contacts</h2>
+                <h2 className="heading">Welcome To All in One Website</h2>
+                <p className="para">asjfaskljfksajfkasdjfkasjfklsajfkasjfa<br></br>
+                    slfjsdklfjsaknfsdfn<br></br>
+                    sflkjsdkfjskdafskdlfasjfd<br></br>
+                    sdfksakjfsaklfj
+                </p>
+            </body>
+        </>
+    );
+}
+
+export default App;
